8523e16d7a2c5e638e249d63d8f9943c3a10abbd
NanJ90 <jianan.beijing@gmail.com>
2024-04-19 12:54:07 -0400

    small dataset created and loading data function works

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import json
+
+def load_data(path:str)-> dict:
+    with open('../data/small_data.json') as f:
+        data = json.load(f)
+    return data
+
+if __name__ == '__main__':
+    #reading JSON file
+    data = load_data('../data/small_data.json')
+        
